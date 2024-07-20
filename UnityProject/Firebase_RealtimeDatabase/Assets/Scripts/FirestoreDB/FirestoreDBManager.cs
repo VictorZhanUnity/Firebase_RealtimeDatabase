3df4b412240d07520f1239b48b6f3337b2eda9e3
VictorZhan <73773024+VictorZhanUnity@users.noreply.github.com>
2024-07-20 09:41:31 +0800

    Update

--- UnityProject/Firebase_RealtimeDatabase/Assets/Scripts/FirestoreDB/FirestoreDBManager.cs
+++ UnityProject/Firebase_RealtimeDatabase/Assets/Scripts/FirestoreDB/FirestoreDBManager.cs
@@ -1,60 +1,182 @@
 using Firebase.Extensions;
 using Firebase.Firestore;
+using System;
 using System.Collections.Generic;
-using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class FirestoreDBManager : MonoBehaviour
+namespace VictorDev.FirebaseUtils
 {
-    private FirebaseFirestore db;
-
-    private ListenerRegistration registration;
-
-    private void Start()
+    public class FirestoreDBManager : MonoBehaviour
     {
-        db = FirebaseFirestore.DefaultInstance;
-        registration = db.Collection("counters").Document("counter").Listen(snapShot =>
+        private FirebaseFirestore dbInstance { get; set; }
+        private Dictionary<string, ListenerRegistration> listenerDict { get; set; } = new Dictionary<string, ListenerRegistration>();
+
+        private void Awake() => dbInstance = FirebaseFirestore.DefaultInstance;
+
+        /// <summary>
+        /// 擷取Collection底下所有的Document與其欄位值
+        /// </summary>
+       public void GetAllDocuments(string collection)
         {
-            Debug.Log($"CallBack");
-            Counter counter = snapShot.ConvertTo<Counter>();
-            Debug.Log($"reg listen: {counter.Count}");
-            txtCounter.SetText( counter.Count.ToString() );
-        });
-    }
-    private void OnDestroy()
-    {
-        registration.Stop();
-    }
+            CollectionReference collectionRef = dbInstance.Collection("collection"); // 替换为你的集合名称
+            collectionRef.GetSnapshotAsync().ContinueWithOnMainThread(task => {
+                if (task.IsCompleted)
+                {
+                    QuerySnapshot snapshot = task.Result;
+                    foreach (DocumentSnapshot document in snapshot.Documents)
+                    {
+                        Debug.Log("Document ID: " + document.Id + ", Document Data: " + document.ToDictionary());
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to get documents: " + task.Exception);
+                }
+            });
+        }
 
-    public TextMeshProUGUI txtCounter;
-    public void OnClick()
-    {
-        int oldCount = int.Parse(txtCounter.text);
-    
 
-
-        Dictionary<string, object> counter = new Dictionary<string, object>()
+        /// <summary>
+        /// 擷取資料項 T:資料型別
+        /// <para>+ 若有值，回傳其型態</para>
+        /// <para>+ 若無值，回傳null</para>
+        /// </summary>
+        public void GetData<T>(string collectionName, string documentId, Action<T> onSuccess, Action onFailed)
         {
-            { "Count", oldCount+1},
-            { "UpdatedBy", "Victor(Dictionary)"},
+            GetDocRef(collectionName, documentId).GetSnapshotAsync()
+                .ContinueWithOnMainThread(task =>
+                {
+                    T result = default(T);
+                    if (task.IsCompleted)
+                    {
+                        if (task.Result.Exists)
+                            result = task.Result.ConvertTo<T>();
+                        onSuccess.Invoke(result);
+                    }
+                    else
+                    {
+                        Debug.LogError("\t [SelectData] Failed to get document: " + task.Exception);
+                    }
+                });
+        }
+        /// <summary>
+        /// 新增一個文件Document
+        /// <para>+ 若資料庫不存在時，會自動新增資料庫</para>
+        /// </summary>
+        /// <param name="data">資料欄位(Dictionary)</param>
+        /// <param name="onSuccessd">成功時回傳其資料項ID值</param>
+        public void CreateDocument(object data, string collection, Action<string> onSuccessed = null, Action onFailed = null)
+        {
+            dbInstance.Collection(collection).AddAsync(data).ContinueWithOnMainThread(
+                task =>
+                {
+                    if (task.IsCompleted)
+                    {
+                        onSuccessed?.Invoke(task.Result.Id);
+                        Debug.Log("\t[CreateDocument] Added document with ID: " + task.Result.Id);
+                    }
+                    else
+                    {
+                        onFailed?.Invoke();
+                        Debug.LogError("\t[CreateDocument] Failed to add document: " + task.Exception);
+                    }
+                });
+        }
+        /// <summary>
+        /// 更新資料 / 新增資料
+        /// <para>+ 若DocumentID值不存在，則變成以DocumentID進行新增資料</para>
+        /// </summary>
+        /// <param name="data">資料項內容</param>
+        /// <param name="collectionName">集合名稱(資料表)</param>
+        /// <param name="documentId">文件名稱(資料ID)</param>
+        public void UpdateData(object data, string collectionName, string documentId, Action<string> onSuccessed = null, Action onFailed = null)
+         => GetDocRef(collectionName, documentId).SetAsync(data).ContinueWithOnMainThread(task =>
+         {
+             if (task.IsCompleted) onSuccessed?.Invoke(documentId);
+             else onFailed?.Invoke();
+         });
+
+        /// <summary>
+        /// 刪除資料Document
+        /// </summary>
+        public void DeleteDocument(string collectionName, string documentId, Action<string> onSuccessed = null, Action onFailed = null)
+        {
+            // 删除 Document
+            GetDocRef(collectionName, documentId).DeleteAsync().ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    onSuccessed?.Invoke(documentId);
+                    Debug.Log("\t[DeleteDocument] Document with ID: " + documentId + " has been deleted.");
+                }
+                else
+                {
+                    onFailed?.Invoke();
+                    Debug.LogError("\t[DeleteDocument] Failed to delete document: " + task.Exception);
+                }
+            });
+        }
+
+        /// <summary>
+        /// 監聽即時同步更新資料
+        /// </summary>
+        public ListenerRegistration RegistraionAsync<T>(string collectionName, string documentId, Action<T> onAsyncUpdate)
+        {
+            ListenerRegistration registration = GetDocRef(collectionName, documentId)
+                .Listen(snapShot => onAsyncUpdate?.Invoke(snapShot.ConvertTo<T>()));
+            listenerDict[string.Concat(collectionName, documentId)] = registration;
+            return registration;
+        }
+
+        /// <summary>
+        /// 取消監聽即時同步更新資料
+        /// </summary>
+        public void CancellRegistraionAsync(string collectionName, string documentId)
+        {
+            string key = string.Concat(collectionName, documentId);
+            if (listenerDict.ContainsKey(key))
+            {
+                listenerDict[key].Stop();
+                listenerDict.Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// 取得Document Ref
+        /// </summary>
+        private DocumentReference GetDocRef(string collectionName, string documentId) => dbInstance.Collection(collectionName).Document(documentId);
+
+        private void OnDestroy()
+        {
+            foreach (string key in listenerDict.Keys)
+            {
+                listenerDict[key].Stop();
+            }
+            listenerDict.Clear();
+        }
+
+        public void AddProduct()
+        {
+            CollectionReference productsRef = dbInstance.Collection("products");
+            Dictionary<string, object> productData = new Dictionary<string, object>
+        {
+            { "Name", "Sample Product" },
+            { "Price", 19.99 },
+            { "Stock", 100 }
         };
 
-        DocumentReference countRef = db.Collection("counters").Document("counter");
-        countRef.SetAsync(counter).ContinueWithOnMainThread(task =>
-        {
-            print("Update Counter");
-        });
-    }
-
-    public void GetData()
-    {
-        db.Collection("counters").Document("counter").GetSnapshotAsync().ContinueWithOnMainThread(task =>
-        {
-            print("Called");
-            Counter counter = task.Result.ConvertTo<Counter>();
-            Debug.Log($"Count:" + counter.Count);
-            txtCounter.SetText(counter.Count.ToString());
-            print(counter.Count.ToString());
-        });
+            productsRef.AddAsync(productData).ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCompleted)
+                {
+                    Debug.Log("Product successfully added!");
+                }
+                else
+                {
+                    Debug.LogError("Failed to add product: " + task.Exception);
+                }
+            });
+        }
     }
 }
